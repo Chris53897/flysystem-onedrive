97dc2b8772a662ffd4b2026764309f226b68bbd7
Wouter de Geringel <118986203+WouterDeGeringel@users.noreply.github.com>
2025-06-20 14:29:07 +0200

    Update OneDriveAdapter.php
    read method now returns stream

--- src/OneDriveAdapter.php
+++ src/OneDriveAdapter.php
@@ -248,14 +248,14 @@ class OneDriveAdapter extends OneDriveUtilityAdapter implements FilesystemAdapte
     /**
      * @throws Exception|GuzzleException
      */
-    public function read(string $path)
+    public function read(string $path):string
     {
         try {
             if (!($stream = $this->readStream($path))) {
                 throw new UnableToReadFile('Unable to read file at ' . $path);
             }
             
-            stream_get_contents($stream);
+            return stream_get_contents($stream);
         } catch (Exception $e) {
             throw new Exception($e);
         }
